7d21fb29271c0ce02a7210b16c695a5b379828d0
Arry-Minati <aryakumar.2002.15sep@gmail.com>
2022-04-06 22:24:53 +0530

    more-seggs

---
+++ calculator.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Calculator{
+
+    class  Program{
+        static void Main(string[] args){
+            Console.WriteLine("Enter type to calculation you need to do");
+            Console.WriteLine("Press 1 to do Addition");
+            Console.WriteLine("Press 2 to do Subtraction");
+            Console.WriteLine("Press 3 to do Division");
+            Console.WriteLine("Press 4 to do Multiplication\n");
+            
+
+        }
+    }
+}
